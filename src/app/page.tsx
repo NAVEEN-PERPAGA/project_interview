da2f9bf3c9ca6ed02664d08b58a2a61107901933
Naveen Perpaga <naveen19perpaga91@gmail.com>
2025-09-19 15:18:42 +0530

    safe website

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,11 @@
 "use client"
 
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import Login from "./login/page";
 
 export default function Home() {
-  const router = useRouter()
-  useEffect(() => {
-    router.push('/login')
-  }, [])
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-
+    <div className="font-sans grid grid-rows-[1fr] items-center justify-items-center  p-8">
+      <Login />
     </div>
   );
 }
